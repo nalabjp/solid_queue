ade10fe7c88832eb05258d6df41e20ed4e2c042f
nalabjp <nalabjp@gmail.com>
2024-05-12 22:30:14 +0900

    Use Hash argument instead of Keyword arguments
    Fix ArgumentError

--- test/dummy/app/jobs/store_result_job.rb
+++ test/dummy/app/jobs/store_result_job.rb
@@ -1,7 +1,12 @@
 class StoreResultJob < ApplicationJob
   queue_as :background
 
-  def perform(value, status: :completed, pause: nil, exception: nil, exit: nil)
+  def perform(value, options = {})
+    status    = options[:status] || :completed
+    pause     = options[:pause]
+    exception = options[:exception]
+    exit      = options[:exit]
+
     result = JobResult.create!(queue_name: queue_name, status: "started", value: value)
 
     sleep(pause) if pause
--- test/dummy/app/jobs/update_result_job.rb
+++ test/dummy/app/jobs/update_result_job.rb
@@ -1,5 +1,9 @@
 class UpdateResultJob < ApplicationJob
-  def perform(job_result, name:, pause: nil, exception: nil)
+  def perform(job_result, options = {})
+    name      = options[:name] || raise(ArgumentError, 'missing keyword: :name')
+    pause     = options[:pause]
+    exception = options[:exception]
+
     job_result.status += "s#{name}"
 
     sleep(pause) if pause
