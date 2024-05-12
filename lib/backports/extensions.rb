f188170c30837a51935971f628f8813c25be520c
nalabjp <nalabjp@gmail.com>
2024-05-12 19:31:04 +0900

    Backport `ActiveRecord::Result.empty` API
    https://github.com/rails/rails/pull/45729

--- lib/backports/extensions.rb
+++ lib/backports/extensions.rb
@@ -13,3 +13,25 @@ module SolidQueueRailsErrorBackport
   end
 end
 Rails.extend(SolidQueueRailsErrorBackport)
+
+# Backport `ActiveRecord::Result.empty` API
+module SolidQueueActiveRecordInsertAllBackport
+  def insert_all(attributes, **_options)
+    if attributes.empty?
+      # https://github.com/rails/rails/blob/v6.1.7.7/activerecord/lib/active_record/insert_all.rb#L11
+      # In AR 6.1.7.7, it raises ArgumentError when `inserts` argument is blank.
+      #
+      # https://github.com/rails/rails/blob/v7.1.3.2/activerecord/lib/active_record/insert_all.rb#L42
+      # But in AR 7.1.3.2, it returns `ActiveRecord::Result.empty`.
+      #
+      # https://github.com/rails/rails/blob/v7.1.3.2/activerecord/lib/active_record/result.rb#L41-L47
+      # https://github.com/rails/rails/blob/v7.1.3.2/activerecord/lib/active_record/result.rb#L195
+      ActiveRecord::Result.new([].freeze, [].freeze, {}.freeze).freeze
+    else
+      super
+    end
+  end
+end
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.extend(SolidQueueActiveRecordInsertAllBackport)
+end
